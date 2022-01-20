bfaaf65a3404389bb54b37524d885a07f927b0bf
Ritchie <ritchiepoh@gmail.com>
2022-01-20 23:45:20 +0800

    Show location blue dot

--- app/src/main/java/com/example/petroles/MapActivity.java
+++ app/src/main/java/com/example/petroles/MapActivity.java
@@ -2,16 +2,39 @@ package com.example.petroles;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    // Global variable declaration
+    private static final String TAG = "MapActivity";
+    private static final float ZOOM = 15f;
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_REQUEST_CODE = 100;
+    private Boolean locationPermissionGranted = false;
+
+    // Object declaration
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private GoogleMap gMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +42,128 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_map);
 
         // Creating a supportMapFragment
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        getLocationPermission();
+        // If and only if the location permission is granted, we will initialize the map
+        if (locationPermissionGranted) {
+            Log.d(TAG, "onCreate: initializing map");
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+        }
+
     }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-        googleMap.addMarker(new MarkerOptions()
-        .position(new LatLng(0, 0))
-        .title("Marker"));
+        Toast.makeText(this, "Map is ready", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onMapReady: Map is ready");
+
+        if (locationPermissionGranted) {
+            getDeviceLocation();
+
+            // Last check on permission before locating on maps
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+
+            googleMap.setMyLocationEnabled(true);
+        }
     }
+
+    private void getDeviceLocation() {
+        Log.d(TAG, "getDeviceLocation: getting device's current location");
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        try {
+            if (locationPermissionGranted) {
+                final Task location = fusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        /*
+                            If things go right, it should be able to get the location
+                            Then get the Latitude and Longitude of the current location
+                            And then move the camera to the current location
+                        */
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "onComplete: location found!");
+                            Location currentLocation = (Location) task.getResult();
+
+                            // Creating a new LatLng object with the current location lat and lng
+                            LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+                            Log.d(TAG, "onComplete: moving camera to lat: " + latLng.latitude +
+                                   ", lng: " + latLng.longitude);
+
+                            /*
+                                Check whether the gMap object is null or not before moving the camera
+                                Because the moveCamera function might be called before the map is ready
+                                and initialized. Causing a NullPointerException
+                            */
+                            if (gMap != null) {
+                                // Move the camera to current location with lat and lng
+                                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, ZOOM));
+                            }
+
+                        } else {
+                            // Log the error message and create a toast to show the error
+                            Log.d(TAG, "onComplete: Unable to get current location, it could be null");
+                            Toast.makeText(MapActivity.this, "Unable to get location", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e) {
+            // Log the error where no permission is granted
+            Log.e(TAG, "getDeviceLocation: SecurityException: " + e.getMessage());
+        }
+    }
+
+    // Function to ask for user's permission to access location
+    private void getLocationPermission() {
+        Log.d(TAG, "getLocationPermission: getting location permissions");
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        /*
+            Nested if to check both fine location and coarse location permission
+            If either one false, will request permission
+         */
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(), FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(), COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                locationPermissionGranted = true;
+            } else {
+                ActivityCompat.requestPermissions(this, permissions, LOCATION_REQUEST_CODE);
+            }
+        } else {
+            ActivityCompat.requestPermissions(this, permissions, LOCATION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG, "onRequestPermission: called");
+        locationPermissionGranted = false;
+
+        switch (requestCode) {
+            case LOCATION_REQUEST_CODE: {
+                if (grantResults.length > 0) {
+                    for (int i=0; i<grantResults.length; i++) {
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                            locationPermissionGranted = false;
+                            Log.d(TAG, "onRequestPermission: permission not granted");
+                            return;
+                        }
+                    }
+
+                    Log.d(TAG, "onRequestPermission: permission granted");
+                    locationPermissionGranted = true;
+                }
+            }
+        }
+    }
+
+
 }
