756c61b0b2bdb3468066935dc6649639c665d5d9
Ming Hui <tiewminghui@gmail.com>
2022-01-26 23:35:32 +0800

    splash screen updated

---
+++ app/src/main/java/com/example/petroles/Splash_Activity.java
@@ -0,0 +1,44 @@
+package com.example.petroles;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash_Activity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN_TIMEOUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.splash_activity);
+
+        Animation fadeOut = new AlphaAnimation(1, 0);
+        fadeOut.setInterpolator(new AccelerateDecelerateInterpolator());
+        fadeOut.setStartOffset(500);
+        fadeOut.setDuration(1800);
+        ImageView image = findViewById(R.id.imageView);
+
+        image.setAnimation(fadeOut);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash_Activity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIMEOUT);
+    }
+}
